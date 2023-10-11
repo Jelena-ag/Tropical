ca0570434ae010d5aa66fe90911ba0268ef680ea
Елена Агеева <jelena.agiejewa@gmail.com>
2023-10-11 23:39:40 +0200

    continuity

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 const nav = document.querySelector('.nav__items');
 const navBtn = document.querySelector('.burger-btn');
-const NavItems = document.querySelectorAll('.nav__item');
+const navItems = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
 
 const showNav = () => {
 	nav.classList.toggle('nav--active');
-
-	NavItems.forEach(item => {
+	// navBtnBars.classList.remove('black-bars-color');
+	navItems.forEach(item => {
 		item.addEventListener('click', () => {
 			nav.classList.remove('nav--active');
 		});
@@ -18,7 +21,7 @@ const showNav = () => {
 const showNavItemsAnimation = () => {
 	let delayTime = 0;
 
-	NavItems.forEach(item => {
+	navItems.forEach(item => {
 		item.classList.toggle('nav-items-animation');
 		item.style.animationDelay = '.' + delayTime + 's';
 		delayTime++;
@@ -26,13 +29,38 @@ const showNavItemsAnimation = () => {
 };
 
 const deleteAnimation = () => {
-	NavItems.forEach(item => {
+	navItems.forEach(item => {
 		item.addEventListener('click', () => {
-			NavItems.forEach(el => {
+			navItems.forEach(el => {
 				el.classList.remove('nav-items-animation');
 			});
 		});
 	});
 };
 
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+	allSections.forEach(section => {
+		if (
+			section.classList.contains('white-section') &&
+			section.offsetTop <= currentSection + 60 &&
+			!nav.classList.contains('nav--active')
+		) {
+			navBtnBars.classList.add('black-bars-color');
+		} else if (
+			!section.classList.contains('white-section') &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.remove('black-bars-color');
+		}
+	});
+};
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
 navBtn.addEventListener('click', showNav);
+window.addEventListener('scroll', handleObserver);
